ff45962c4c9c568f761909755d913fad91bdf4c6
rachitth <rachit.thakur1296@gmail.com>
2023-11-11 18:46:06 -0500

    Aerial functions definition

--- include/aerial_robot.h
+++ include/aerial_robot.h
@@ -1,28 +1,26 @@
-// #pragma once
-// #include "mobile_robot.h"
-// #include <cmath>
-// #include <iostream>
-// #include <utility>
+#pragma once
+#include "mobile_robot.h"
+#include <cmath>
+#include <iostream>
+#include <utility>
 
-// namespace RWA2 {
-// class AerialRobot : public MobileRobot {
-// private:
-//   bool has_wings_;
-//   double altitude_{0.0};
-//   bool is_flying_{false};
-//   void take_off(double altitude_);
-//   void land();
+namespace RWA2 {
+class AerialRobot : public MobileRobot {
+private:
+  bool has_wings_;
+  double altitude_{0.0};
+  bool is_flying_{false};
+  void take_off(double altitude_);
+  void land();
 
-// public:
+public:
+  AerialRobot(double x, double y, double orientation, double speed,
+               std::string model, std::string sensor_model, std::string battery_model, int battery_charge, bool has_wings, double altitude, bool is_flying)
+      : MobileRobot(x, y, orientation, speed, model, sensor_model, battery_model, battery_charge), has_wings_{has_wings}, altitude_{altitude}, is_flying_{is_flying} {}
+  void move(double distance, double angle) override;
+  void print_Status() override;
 
-//   void move(double distance, double angle);
-//   void print_Status();
-
-//   AerialRobot(double x, double y, double orientation, double speed, std::string model, bool has_wings, double altitude, bool is_flying)
-//       : MobileRobot(x, y, orientation, speed, model), has_wings_{has_wings},
-//         altitude_{altitude}, is_flying_{is_flying} {}
-
-// protected:
-//   void rotate(double angle);
-// };
-// } // namespace RWA2
+protected:
+  void rotate(double angle) override;
+};
+} // namespace RWA2
--- include/aquatic_robot.h
+++ include/aquatic_robot.h
@@ -1,28 +1,26 @@
-// #pragma once
-// #include "mobile_robot.h"
-// #include <cmath>
-// #include <iostream>
-// #include <utility>
+#pragma once
+#include "mobile_robot.h"
+#include <cmath>
+#include <iostream>
+#include <utility>
 
-// namespace RWA2 {
-// class AquaticRobot : public MobileRobot {
-// private:
-//   bool has_fins_;
-//   double depth_{0.0};
-//   bool is_diving_{false};
-//   void dive(double depth);
-//   void surface();
+namespace RWA2 {
+class AquaticRobot : public MobileRobot {
+private:
+  bool has_fins_;
+  double depth_{0.0};
+  bool is_diving_{false};
+  void dive(double depth);
+  void surface();
 
-// public:
+public:
+  AquaticRobot(double x, double y, double orientation, double speed,
+               std::string model, std::string sensor_model, std::string battery_model, int battery_charge, bool has_fins, double depth, bool is_diving)
+      : MobileRobot(x, y, orientation, speed, model, sensor_model, battery_model, battery_charge), has_fins_{has_fins}, depth_{depth}, is_diving_{is_diving} {}
+  void move(double distance, double angle) override;
+  void print_Status() override;
 
-//   void move(double distance, double angle);
-//   void print_Status();
-
-//   AquaticRobot(double x, double y, double orientation, double speed, std::string model, bool has_fins, double depth, bool is_diving)
-//       : MobileRobot(x, y, orientation, speed, model), has_fins_{has_fins},
-//         depth_{depth}, is_diving_{is_diving} {}
-
-// protected:
-//   void rotate(double angle);
-// };
-// } // namespace RWA2
+protected:
+  void rotate(double angle) override;
+};
+} // namespace RWA2
