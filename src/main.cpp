0367ac9e8a7dd4270e4e5dd624238a418994c46a
obdurhmn <obdurhmn@umd.edu>
2023-11-12 17:22:34 -0500

    Revert "Added Function Wrapper"
    This reverts commit fbb12f89d2c9eb7890753116298cbe5324c0e7b4.

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,14 @@
 #include <memory>
 #include <vector>
 
-// int main() {
-//   auto wheeled_robot = std::make_unique<RWA2::WheeledRobot>(
-//       0.0, 0.0, 0.0, 0.0, "Turtlebot", "ss", 5, 10);
-//   wheeled_robot->move(2, 90.0);
-//   wheeled_robot->move(2, 90.0);
-//   wheeled_robot->move(200, 90.0);
-//   auto legged_robot = std::make_unique<RWA2::LeggedRobot>(
-//       0.0, 0.0, 0.0, 0.0, "Atlas", "ss", 100, 0.5);
-//   legged_robot->move(2.0, 90.0);
-//   return 0;
-// }
-
-void moveRobot(RWA2::MobileRobot* robot, double distance, double angle) {
-    robot->move(distance, angle);
-}
-
 int main() {
   auto wheeled_robot = std::make_unique<RWA2::WheeledRobot>(
       0.0, 0.0, 0.0, 0.0, "Turtlebot", "ss", 5, 10);
-  moveRobot(wheeled_robot.get(), 2, 90.0);
-  moveRobot(wheeled_robot.get(), 2, 90.0);
-  moveRobot(wheeled_robot.get(), 200, 90.0);
+  wheeled_robot->move(2, 90.0);
+  wheeled_robot->move(2, 90.0);
+  wheeled_robot->move(200, 90.0);
   auto legged_robot = std::make_unique<RWA2::LeggedRobot>(
       0.0, 0.0, 0.0, 0.0, "Atlas", "ss", 100, 0.5);
-  moveRobot(legged_robot.get(), 2.0, 90.0);
+  legged_robot->move(2.0, 90.0);
   return 0;
 }
