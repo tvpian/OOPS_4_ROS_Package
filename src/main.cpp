a4920148960566ab00bfd4ca1aa524dd8ef9e4de
Tharun Puthanveettil <tharunv.pvit@yahoo.com>
2023-11-12 13:04:20 -0500

    Handled few housekeeping issues

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@
 int main() {
   auto wheeled_robot = std::make_unique<RWA2::WheeledRobot>(
       0.0, 0.0, 0.0, 0.0, "Turtlebot", "ss", 5, 10);
-  wheeled_robot->move(10, 90.0);
+  wheeled_robot->move(2, 90.0);
+  wheeled_robot->move(2, 90.0);
+  wheeled_robot->move(200, 90.0);
   auto legged_robot = std::make_unique<RWA2::LeggedRobot>(
-      0.0, 0.0, 0.0, 0.0, "Atlas", "ss", 300, 0.5);
-  legged_robot->move(5.0, 90.0);
+      0.0, 0.0, 0.0, 0.0, "Atlas", "ss", 100, 0.5);
+  legged_robot->move(2.0, 90.0);
   return 0;
 }
