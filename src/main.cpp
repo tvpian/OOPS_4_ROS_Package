22fc48cbd4eaf99fefc53a4526a71064ff9f5219
Tharun Puthanveettil <tharunv.pvit@yahoo.com>
2023-11-12 11:59:14 -0500

    Modified the main to meet the requirements: 'Keep only move()'

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,10 @@
 
 int main() {
   auto wheeled_robot = std::make_unique<RWA2::WheeledRobot>(
-      0.0, 0.0, 0.0, 0.0, "Turtlebot", "Lithium", 100, 10);
-  // Add a sensor to the robot
-  wheeled_robot->add_Sensor(std::make_unique<RWA2::Sensor>("Lidar"));
-  // Add another sensor to the robot
-  wheeled_robot->add_Sensor(std::make_unique<RWA2::Sensor>("Camera"));
-  wheeled_robot->move(5, 90.0);
+      0.0, 0.0, 0.0, 0.0, "Turtlebot", "ss", 5, 10);
+  wheeled_robot->move(10, 90.0);
   auto legged_robot = std::make_unique<RWA2::LeggedRobot>(
-      0.0, 0.0, 0.0, 0.0, "Atlas", "Lithium", 300, 0.5);
-  // Add a sensor to the robot
-  legged_robot->add_Sensor(std::make_unique<RWA2::Sensor>("Lidar"));
-  // Add another sensor to the robot
-  legged_robot->add_Sensor(std::make_unique<RWA2::Sensor>("Camera"));
+      0.0, 0.0, 0.0, 0.0, "Atlas", "ss", 300, 0.5);
   legged_robot->move(5.0, 90.0);
-  legged_robot->move(1.0, 340.0);
   return 0;
 }
